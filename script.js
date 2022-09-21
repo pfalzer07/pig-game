bb668c77d894d624901edca85fe762fa12dec0f8
Ahmed <ahmedlekin.97@gmail.com>
2022-09-22 00:03:46 +0300

    implement dice rolling

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+//Selecting elements
+const score0 = document.querySelector('#score--0');
+const score1 = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+//New game
+score0.textContent = 0;
+score1.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+//Rolling dice
+btnRoll.addEventListener('click', function () {
+  //Generate random roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  //Display dice
+  diceEl.classList.remove('hidden');
+  console.log(dice);
+  diceEl.src = `dice-${dice}.png`;
+});
